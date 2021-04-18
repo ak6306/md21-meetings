3848a5b40e0cf0586c2c4a4bd5fbfdd926e27796
Aditya Choudhary <ak6306@gmail.com>
2021-04-19 05:29:04 +0530

    Init

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = 'mongodb+srv://ak6306:shashi123@cluster0.bqugn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
 const connection = mongoose.connection;
 connection.once('open', ()=>{
