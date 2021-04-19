5af89bf0f8e64bd64730ad155e178c4abcae265e
Aditya Choudhary <ak6306@gmail.com>
2021-04-19 06:17:02 +0530

    proxy

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const proxy = require("http-proxy-middleware");
 
+module.exports = app => {
+  app.use(proxy("/*", { target: "http://localhost:5000/" }));
+};
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
@@ -23,6 +27,7 @@ app.use('/meets', meetRouter);
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
 }
+
 app.listen(port, ()=>{
     console.log(`Server running on port: ${port}`)
 })
